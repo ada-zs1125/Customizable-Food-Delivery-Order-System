9922f184c465ecdeb3beed7370c28ecab380b1fd
ada-zs1125 <zeyu.sun25@imperial.ac.uk>
2026-02-12 17:41:08 +0000

    feat: add dish pagination query function

--- sky-take-out/sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-take-out/sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -1,16 +1,15 @@
 package com.sky.controller.admin;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.DishService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static java.util.ResourceBundle.clearCache;
 
@@ -18,6 +17,8 @@ import static java.util.ResourceBundle.clearCache;
 @RequestMapping("/admin/dish")
 @Api(tags = "菜品相关接口")
 @Slf4j
+
+
 public class DishController {
 
     @Autowired
@@ -29,13 +30,26 @@ public class DishController {
      * @return
      */
     @PostMapping()
-    @ApiOperation("新增菜品")
+    @ApiOperation("Add new dish")
     public Result save(@RequestBody DishDTO dishDTO) {
         log.info("新增菜品：{}", dishDTO);
         dishService.saveWithFlavor(dishDTO);
 
 
         return Result.success();
+    }
+
+    /**
+     * 菜品分页查询
+     * @param dishPageQueryDTO
+     * @return
+     */
+    @GetMapping("/page")
+    @ApiOperation("Menu pagination query")
+    public Result<PageResult> page(DishPageQueryDTO dishPageQueryDTO) {
+        log.info("菜品分页查询:{}", dishPageQueryDTO);
+        PageResult pageResult = dishService.pageQuery(dishPageQueryDTO);//后绪步骤定义
+        return Result.success(pageResult);
     }
 
 }
